7772c16193370f2c3aed34de7375f7d7b1b7115d
pydevdse <pydev.dse@gmail.com>
2022-01-27 22:12:20 +0200

    parser

--- rozetka/pipelines.py
+++ rozetka/pipelines.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import pymongo
 from scrapy.exceptions import DropItem
@@ -5,13 +6,13 @@ from scrapy.exceptions import DropItem
 
 class RozetkaPipeline(object):
     def __init__(self):
-        db_name = os.getenv('DB_NAME')
+        db_name = os.getenv('DB_NAME') if os.getenv('DB_NAME') else "rozetka"
         db_user = os.getenv('DB_USERNAME')
         db_pass = os.getenv('DB_PASSWORD')
         db_host = os.getenv('DB_HOST')
         db_port = os.getenv('DB_PORT')
-        client = pymongo.MongoClient(f"mongodb://{db_host}:{db_port}/{db_name}")
-        # client = pymongo.MongoClient(f"mongodb://{db_user}:{db_pass}}@{db_host}:{db_port}}/{db_name}
+        #client = pymongo.MongoClient("mongodb://localhost:27017/rozetka")
+        client = pymongo.MongoClient(f"mongodb://{db_host}")
         self.db = client[db_name]
         self.collection = self.db['product']
 
@@ -19,5 +20,6 @@ class RozetkaPipeline(object):
         for data in item:
             if not data:
                 raise DropItem("Missing {0}!".format(data))
+        #logging.info(str(item))
         index = self.db.product.insert(dict(item))
         return f"index: {index}"
--- rozetka/spiders/rozetkaspider.py
+++ rozetka/spiders/rozetkaspider.py
@@ -1,26 +1,46 @@
+import logging
 import json
 import scrapy
 from ..items import RozetkaItem
 
+logging.basicConfig(level=logging.INFO)
+
 class RozetkaSpider(scrapy.Spider):
     name = 'rozetka'
     start_urls = ['https://hard.rozetka.com.ua/hdd/c80084/']
-    COUNT_PAGES = 1
-    
+    COUNT_PAGES = 2
+    #url = 'https://hard.rozetka.com.ua/hdd/c80084/'
+    """
+    def start_requests(self):
+        response = scrapy.Request(self.url)
+        #pages = response.xpath('//li[@class="pagination__item ng-star-inserted"]/text()')
+        #print(pages[len(pages)-1].get())
+        print(dir(response))
+        return
+    """
+
     def parse(self, response):
+        pages = response.xpath('//a[@class="pagination__link ng-star-inserted"]//text()')
+        pages = int(pages[-1].get()) if len(pages)>3 else None
+        if not pages:
+            return
+        logging.info(f'PAGES: {pages}')
+        for page in range(1, pages+1)[:2]:
+            url = self.start_urls[0] + f'page={page}/'
+            yield scrapy.Request(url, callback=self.parse_page)
+
+    def parse_page(self, response):
         for title in response.xpath('.//a[@class="goods-tile__heading ng-star-inserted"]'):
-            yield response.follow(url=title.xpath('.//@href').extract_first(), callback=self.parse_product)
-        if self.COUNT_PAGES > 0:
-            self.COUNT_PAGES -= 1
-            next_page = response.xpath('//a[@class="button button_color_gray button_size_medium pagination__direction pagination__direction_type_forward ng-star-inserted"]/@href').extract_first()
-            if next_page:
-                yield response.follow(next_page, self.parse)
+            url = title.xpath('.//@href').extract_first()
+            #logging.info(url)
+            yield scrapy.Request(url, callback=self.parse_product)
+
 
     def parse_product(self, response):
         product_json = json.loads(response.xpath('.//script[@data-seo="Product"]/text()').extract_first())
         meta = response.meta
         meta['product'] = product_json
-        yield response.follow(response.url+'characteristics/', meta=meta, callback=self.parse_characteristics)
+        yield scrapy.Request(response.url+'characteristics/', meta=meta, callback=self.parse_characteristics)
 
     def parse_characteristics(self, response):
         product_json = response.meta['product']
